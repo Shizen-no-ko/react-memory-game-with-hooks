6e273f7ac21e4c1cfe7b303ae0b0fe21d3bb9948
Mike Search <search_michael@yahoo.com>
2021-10-24 09:05:32 +0100

    Refactors and comments Columns.js

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -1,19 +1,16 @@
 import Row from './Row';
 import styles from './styles/Column.module.css';
 
-
-
-
+// map passed-down rows and render row components accordingly, passing down useful state
 function Columns(props) {
     return (
         <div className={styles.container}>
             <div className={styles.column}>
                 {props.rows.map((row, i) =>
-                    <Row key={i} row={row} getDataFromTile={props.getDataFromTile} turned={props.turned} matched={props.matched} reset={props.reset} clearReset={props.clearReset}/>
+                    <Row key={i} row={row} getDataFromTile={props.getDataFromTile} turned={props.turned} matched={props.matched} reset={props.reset} clearReset={props.clearReset} />
                 )}
             </div>
         </div>
-
     )
 };
 
