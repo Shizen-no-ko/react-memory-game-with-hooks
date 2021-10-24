82b64358357255e8da4ed8634e02d9b91943f20f
Mike Search <search_michael@yahoo.com>
2021-10-24 09:23:46 +0100

    Refactors and Comments Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,8 @@ import styles from './styles/Title.module.css';
 function Title(props) {
     return (
         <div className={styles.container}>
-        <h1 className={(props.left ? styles.textLeft : styles.textRight)} >MEMORY</h1>
+            {/* conditional application of styles according to whether left or not */}
+            <h1 className={(props.left ? styles.textLeft : styles.textRight)} >MEMORY</h1>
         </div>
     )
 };
