826451e95a01c9274c56900fcffdac5b14b8a2cf
Mike Search <search_michael@yahoo.com>
2021-10-24 09:25:12 +0100

    Refactors and comments Row.js

--- src/components/Row.js
+++ src/components/Row.js
@@ -5,9 +5,8 @@ import styles from './styles/Row.module.css';
 function Row(props) {
     return (
         <div className={styles.row}>
-        {props.row.map((item, i) => <Tile key = {i} item = {item} getDataFromTile={props.getDataFromTile} turned={props.turned} matched={props.matched} reset={props.reset} clearReset={props.clearReset}/>)}
+            {props.row.map((item, i) => <Tile key={i} item={item} getDataFromTile={props.getDataFromTile} turned={props.turned} matched={props.matched} reset={props.reset} clearReset={props.clearReset} />)}
         </div>
-
     )
 };
 
