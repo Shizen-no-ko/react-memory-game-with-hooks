338fff8b7b57ac21b97978149fc7c97d13988aa8
Mike Search <search_michael@yahoo.com>
2021-10-24 09:21:22 +0100

    Refactors and Comments ScoreDisplay.js

--- src/components/ScoreDisplay.js
+++ src/components/ScoreDisplay.js
@@ -1,11 +1,11 @@
 import styles from './styles/ScoreDisplay.module.css';
 
-
+// variable component for displaying tries and pairs matched scores
 function ScoreDisplay(props) {
     return (
         <div className={(props.left ? styles.left : styles.right)}>
-        <h3 className={styles.h3}>{props.text}</h3>
-        <h1 className={styles.h1}>{props.tries}</h1>
+            <h3 className={styles.h3}>{props.text}</h3>
+            <h1 className={styles.h1}>{props.tries}</h1>
         </div>
     )
 };
