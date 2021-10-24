f77a2e066a152bbdf7051b5d07bbc4339a09e170
Mike Search <search_michael@yahoo.com>
2021-10-24 09:16:00 +0100

    Refactors and comments Container.js

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import styles from './styles/Container.module.css';
 
@@ -6,23 +6,23 @@ import Game from './Game';
 import GameOver from "./GameOver";
 
 
-
 function Container() {
-
-    const [gameOver, setgameOver] = useState(false); 
-
-    const checkGameOver = (gameOver)=> {
-        if(gameOver){
+    // state for managing gameOver condition
+    const [gameOver, setgameOver] = useState(false);
+    // function for retrieving gameOver state from the Game.js module
+    const checkGameOver = (gameOver) => {
+        if (gameOver) {
             setgameOver(true);
         }
     }
 
+    // rendering of Game module and conditional rendering of gameOver text
     return (
         <div className={styles.container}>
-        {gameOver ? <GameOver/> :
-        <div></div>
-    }  
-        <Game checkGameOver={checkGameOver} />   
+            {gameOver ? <GameOver /> :
+                <div></div>
+            }
+            <Game checkGameOver={checkGameOver} />
         </div>
     )
 };
