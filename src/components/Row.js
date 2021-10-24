aaae0b4e6cf5aa16df4b54d35fa89427124ecccc
Mike Search <search_michael@yahoo.com>
2021-10-24 09:12:34 +0100

    Refactors and comments Row.js

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,11 @@
 import Tile from './Tile';
 import styles from './styles/Row.module.css';
 
+// map passed down rows into tiles
 function Row(props) {
     return (
         <div className={styles.row}>
-        {props.row.map((item, i) => <Tile key = {i} keyCheck = {i} item = {item} getDataFromTile={props.getDataFromTile} turned={props.turned} matched={props.matched} reset={props.reset} clearReset={props.clearReset}/>)}
+        {props.row.map((item, i) => <Tile key = {i} item = {item} getDataFromTile={props.getDataFromTile} turned={props.turned} matched={props.matched} reset={props.reset} clearReset={props.clearReset}/>)}
         </div>
 
     )
